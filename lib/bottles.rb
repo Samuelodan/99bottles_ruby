44b1ca73d40254c4f6f51dd762114fcc63875d17
Samuel ODaniels <samodan195@gmail.com>
2022-07-11 23:32:33 +0100

    Change if statement in Bottles#verse to switch

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,26 +2,27 @@
 
 class Bottles
   def verse(num)
-    if num > 2
-      "#{num} bottles of beer on the wall, " \
-      "#{num} bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "#{num - 1} bottles of beer on the wall.\n"
-    elsif num == 2
-      "#{num} bottles of beer on the wall, " \
-      "#{num} bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "#{num - 1} bottle of beer on the wall.\n"
-    elsif num == 1
-      "#{num} bottle of beer on the wall, " \
-      "#{num} bottle of beer.\n" \
-      'Take it down and pass it around, ' \
-      "no more bottles of beer on the wall.\n"
-    elsif num.zero?
+    case num
+    when 0
       'No more bottles of beer on the wall, ' \
       "no more bottles of beer.\n" \
       'Go to the store and buy some more, ' \
       "99 bottles of beer on the wall.\n"
+    when 1
+      "#{num} bottle of beer on the wall, " \
+      "#{num} bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
+    when 2
+      "#{num} bottles of beer on the wall, " \
+      "#{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottle of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, " \
+      "#{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottles of beer on the wall.\n"
     end
   end
 
