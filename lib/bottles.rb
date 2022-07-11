3cfbaa2730f4b41c10acd62cf0a41cfdfc79205c
Samuel ODaniels <samodan195@gmail.com>
2022-07-11 15:10:42 +0100

    Create Bottles class and methods to pass all tests
    this is my first attempt that I wrote in about 30 mins. I'm commiting now so I can come back to take a look after all the refactoring.

---
+++ lib/bottles.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Bottles
+  def verse(num)
+    if num > 2
+      "#{num} bottles of beer on the wall, " \
+      "#{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottles of beer on the wall.\n"
+    elsif num == 2
+      "#{num} bottles of beer on the wall, " \
+      "#{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottle of beer on the wall.\n"
+    elsif num == 1
+      "#{num} bottle of beer on the wall, " \
+      "#{num} bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
+    elsif num.zero?
+      'No more bottles of beer on the wall, ' \
+      "no more bottles of beer.\n" \
+      'Go to the store and buy some more, ' \
+      "99 bottles of beer on the wall.\n"
+    end
+  end
+
+  def verses(num1, num2)
+    diff = (num1 - num2) + 1
+    result = ''
+    start = num1
+    diff.times do |count|
+      result += verse(start)
+      result += "\n" if (diff - count) > 1
+      start -= 1
+    end
+    result
+  end
+
+  def song
+    verses(99, 0)
+  end
+end
