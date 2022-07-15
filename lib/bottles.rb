f4086213d30eb5e4cbbb32db87c8e270f284c560
Samuel ODaniels <samodan195@gmail.com>
2022-07-15 18:28:10 +0100

    Make style changes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
+
 require_relative './bottle_number'
 require_relative './bottle_number0'
-
 
 class Bottles
   def verse(num)
     bottle_number = bottle_number_for(num)
     next_bottle_number = bottle_number_for(bottle_number.successor)
-  
+
     "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" \
-    "#{bottle_number.action}, " \
-    "#{next_bottle_number} of beer on the wall.\n"
+      "#{bottle_number} of beer.\n" \
+      "#{bottle_number.action}, " \
+      "#{next_bottle_number} of beer on the wall.\n"
   end
 
   def verses(num1, num2)
@@ -23,7 +23,7 @@ class Bottles
   end
 
   def bottle_number_for(num)
-    if num == 0
+    if num.zero?
       BottleNumber0
     else
       BottleNumber
