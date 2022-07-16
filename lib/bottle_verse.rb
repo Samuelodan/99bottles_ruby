edd44427282186266d9288690ab448e3e2bcb6db
Samuel ODaniels <samodan195@gmail.com>
2022-07-16 23:40:26 +0100

    Copy original #verse to BottleVerse#verse

--- lib/bottle_verse.rb
+++ lib/bottle_verse.rb
@@ -6,4 +6,14 @@ class BottleVerse
   def initialize(num)
     @num = num
   end
+
+  def verse(num)
+    bottle_number = BottleNumber.for(num)
+    next_bottle_number = bottle_number.successor
+
+    "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" \
+      "#{bottle_number.action}, " \
+      "#{next_bottle_number} of beer on the wall.\n"
+  end
 end
