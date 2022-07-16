4e067c3a0fd1b35c86d05e4d346e29c6bae21a96
Samuel ODaniels <samodan195@gmail.com>
2022-07-17 00:42:37 +0100

    Remove unnecessary variable

--- lib/bottle_verse.rb
+++ lib/bottle_verse.rb
@@ -9,11 +9,10 @@ class BottleVerse
 
   def lyrics
     bottle_number = BottleNumber.for(num)
-    next_bottle_number = bottle_number.successor
 
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" \
       "#{bottle_number.action}, " \
-      "#{next_bottle_number} of beer on the wall.\n"
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
