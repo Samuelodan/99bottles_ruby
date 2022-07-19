aa65ed15d995b84a9766d2240cce65265957383f
Samuel ODaniels <samodan195@gmail.com>
2022-07-20 00:06:27 +0100

    Move two tests from BottlesTest to BottleVerseTest

--- test/bottle_verse_test.rb
+++ test/bottle_verse_test.rb
@@ -12,4 +12,22 @@ class BottleVerseTest < Minitest::Test
       "98 bottles of beer on the wall.\n"
     assert_equal expected, BottleVerse.lyrics(99)
   end
+
+  def test_another_verse
+    expected =
+      "3 bottles of beer on the wall, " +
+      "3 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of beer on the wall.\n"
+    assert_equal expected, BottleVerse.lyrics(3)
+  end
+
+  def test_verse_2
+    expected =
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    assert_equal expected, BottleVerse.lyrics(2)
+  end
 end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -4,24 +4,6 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
-  def test_another_verse
-    expected =
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(3)
-  end
-
-  def test_verse_2
-    expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(2)
-  end
-
   def test_verse_1
     expected =
       "1 bottle of beer on the wall, " +
