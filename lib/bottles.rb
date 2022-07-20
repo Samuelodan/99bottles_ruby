12b3ed17848583c134a8177452e88ff5a56118ea
Samuel ODaniels <samodan195@gmail.com>
2022-07-20 17:09:45 +0100

    Remove deprecated Bottles class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,26 +6,6 @@ require_relative './bottle_number1'
 require_relative './bottle_number6'
 require_relative './bottle_verse'
 
-class Bottles
-  attr_reader :verse_template
-
-  def initialize(verse_template: BottleVerse)
-    @verse_template = verse_template
-  end
-
-  def verse(num)
-    verse_template.lyrics(num)
-  end
-
-  def verses(num1, num2)
-    num1.downto(num2).map { |num| verse(num) }.join("\n")
-  end
-
-  def song
-    verses(99, 0)
-  end
-end
-
 class CountdownSong
   attr_reader :verse_template
 
